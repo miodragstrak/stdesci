70eaa8ed20cd8c8111e860ba43e2812916a3fc38
miodragstrak <miodrag.strak@gmailcom>
2025-04-09 19:18:02 +0200

    Index Commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-import path from "path";
 
-// Učitaj .env fajl
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
+// Export konfiguracije
 export default defineConfig({
   plugins: [react()],
   define: {
+    // Vite automatski koristi `import.meta.env` za varijable iz .env fajlova
     "process.env": process.env,
   },
+  // Ako koristiš podputanje, možeš postaviti base
+  base: "/", // Postavi putanju ako je aplikacija u poddirektorijumu
 });
