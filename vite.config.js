f103ba13cc90080c5d56e663ff805d3fdc5571e2
miodragstrak <miodrag.strak@gmailcom>
2025-04-09 19:59:40 +0200

    Onako Commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
+import { Buffer } from 'buffer';
 
-// Export konfiguracije
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    // Vite automatski koristi `import.meta.env` za varijable iz .env fajlova
-    "process.env": process.env,
+  plugins: [react(), commonjs()],
+  build: {
+    rollupOptions: {
+      external: [
+        'eventemitter3',  // Dodaj ovu biblioteku kao eksternu ako je potrebno
+        'buffer', // Dodaj buffer kao eksternu
+        '@solana/web3.js',  // Dodaj @solana/web3.js kao eksterni modul
+      ],
+      output: {
+        globals: {
+          eventemitter3: 'EventEmitter',
+          buffer: 'Buffer',  // Ovdje definiši globalnu vrednost za Buffer
+          '@solana/web3.js': 'solanaWeb3',  // Definiši @solana/web3.js kao globalnu varijablu
+        },
+      },
+    },
   },
-  // Ako koristiš podputanje, možeš postaviti base
-  base: "/", // Postavi putanju ako je aplikacija u poddirektorijumu
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'fix-buffer',
+          setup(build) {
+            build.onResolve({ filter: /^buffer$/ }, args => {
+              return { path: args.path, external: true };
+            });
+          },
+        },
+      ],
+    },
+  },
+  define: {
+    global: 'window',  // Osiguraj da global objekat bude definisan
+    Buffer: 'buffer.Buffer',  // Osiguraj da je Buffer dostupan u browser-u
+  },
 });
